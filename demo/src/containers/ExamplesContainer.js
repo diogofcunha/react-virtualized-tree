c3e75689c3b61f546e54a7d5e2490247639566f4
diogofcunha <diogofncunha@gmail.com>
2018-01-07 14:49:34 +0000

    World cup groups example

--- demo/src/containers/ExamplesContainer.js
+++ demo/src/containers/ExamplesContainer.js
@@ -10,52 +10,55 @@ import './ExamplesContainer.css';
 export default class ExamplesContainer extends Component {
   render() {    
     return (
-      <Grid>
-        <Grid.Column width={4}>
-          <Menu fluid vertical tabular>
-            {
-              Object.keys(examples).map(path => (
-                <Link to={`/examples/${path}`} key={path}>
-                  <Menu.Item
-                    name={examples[path].name}
-                    active={this.props.location.pathname === `/examples/${path}`} 
-                  />
-                </Link>)
-              )
-            }
-          </Menu>
-        </Grid.Column>
-
-        <Grid.Column width={10} heigth={1000}>
-            <Route
-              path="/examples/:example" 
-              render={
-                p => {
-                  const selectedExample = examples[p.match.params.example];
-                  const { component: Component, name, description, fileName } = selectedExample;
-            
-                  return (
-                    <div>
-                      <span
-                        className='jump-to-source'>
-                        <a href={getExamplePath(fileName)}>
-                          Jump to source
-                        </a>
-                      </span>
-                      <Header as='h1'>
-                        { name }
-                      </Header>
-                      { description && <Segment>{ description }</Segment> }
-                      <div style={{ height: 500 }}>
-                        <Component {...p}/>
-                      </div>
-                    </div>
-                  );
-                }
+      <div>
+        <Header>Examples</Header>
+        <Grid>
+          <Grid.Column width={4}>
+            <Menu fluid vertical tabular>
+              {
+                Object.keys(examples).map(path => (
+                  <Link to={`/examples/${path}`} key={path}>
+                    <Menu.Item
+                      name={examples[path].name}
+                      active={this.props.location.pathname === `/examples/${path}`} 
+                    />
+                  </Link>)
+                )
               }
-            />
-        </Grid.Column>
-      </Grid>
+            </Menu>
+          </Grid.Column>
+
+          <Grid.Column width={10} heigth={1000}>
+              <Route
+                path="/examples/:example" 
+                render={
+                  p => {
+                    const selectedExample = examples[p.match.params.example];
+                    const { component: Component, name, description, fileName } = selectedExample;
+              
+                    return (
+                      <div>
+                        <span
+                          className='jump-to-source'>
+                          <a href={getExamplePath(fileName)}>
+                            Jump to source
+                          </a>
+                        </span>
+                        <Header as='h1'>
+                          { name }
+                        </Header>
+                        { description && <Segment>{ description }</Segment> }
+                        <div style={{ height: 500 }}>
+                          <Component {...p}/>
+                        </div>
+                      </div>
+                    );
+                  }
+                }
+              />
+          </Grid.Column>
+        </Grid>
+      </div>
     )
   }
 }
--- demo/src/examples/index.js
+++ demo/src/examples/index.js
@@ -14,11 +14,11 @@ const createEntry = (key, fileName, name, description) => ({
  
 export default {
   ...createEntry('basic-tree', 'Basic', 'Basic Tree'),
-  ...createEntry('renderers', 'Renderers', 'Custom Renderer'),
+  ...createEntry('renderers', 'Renderers', 'Create a custom renderer'),
   ...createEntry(
     'customize-renderers',
     'ChangeRenderers',
-    'Customize Default Renderers',
+    'Customize default renderers',
     (<div>
       <p>A good example of a possible customization of a default renderer is customizing the tree to display as a folder structure.</p>
       
@@ -26,6 +26,15 @@ export default {
       {'{ '}<code>
         expanded: string; collapsed: string; lastChild: string;
       </code>{' }'}
+    </div>)
+  ),
+  ...createEntry(
+    'world-cup',
+    'WorldCup',
+    'World cup groups',
+    (<div>
+      <p>FIFA world cup is back in 2018, in this special example the tree view is used to display the group stage draw results!</p>
+      <p>Let the best team win.</p>
     </div>)
   )
 }

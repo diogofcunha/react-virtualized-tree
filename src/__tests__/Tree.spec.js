8100afdb22936973e363ccc20258a39673b0ce26
diogofcunha <diogofncunha@gmail.com>
2018-01-20 00:06:32 +0000

    Increased test coverage

--- src/__tests__/Tree.spec.js
+++ src/__tests__/Tree.spec.js
@@ -1,20 +1,48 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { List } from 'react-virtualized';
 
 import Tree from '../Tree';
 import { Nodes } from '../../testData/sampleTree';
 import { getFlattenedTree } from '../selectors/getFlattenedTree';
 
 describe('Tree', () => {
-  it('should render a Tree with children and the correct props', () => {
-    const nodes = getFlattenedTree(Nodes);
+  const SampleRenderer = ({ node }) => (
+    <div>
+      {`${node.id}`}
+    </div>
+  );
 
-    const wrapper = shallow(
-      <Tree nodes={nodes}>
-        Hello!
-      </Tree>
-    );
+  const nodes = getFlattenedTree(Nodes);
+
+  const setup = () => shallow(
+    <Tree nodes={nodes} onChange={jest.fn()}>
+      {
+        ({ node, ...rest }) =>
+          <SampleRenderer
+            node={node}
+          >
+            { node.name }
+          </SampleRenderer>
+      }
+    </Tree>
+  );
+
+  it('should render a Tree with children and the correct props', () => {    
+    const wrapper = setup();
 
     expect(wrapper.render()).toMatchSnapshot();
+  });
+
+  it('rowRenderer should render the Node renderer with the correct props', () => {
+    const wrapper = setup();
+
+    const { rowRenderer: rowRendererCb } = wrapper.instance();
+    const RowRenderer = rowRendererCb(nodes);
+    const rowRendererProps = { key: 'a', index: 0, style: {} };
+    
+    const rowRendererWrapper = shallow(<RowRenderer {...rowRendererProps}/>);
+
+    expect(rowRendererWrapper.first().children().props()).toMatchSnapshot();
   });
 });

5a2f07809ad1b3c40e139c7f2a55a7d3d0672dae
diogofcunha <diogofncunha@gmail.com>
2018-01-28 18:08:02 +0000

    FilteringContainer release

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Tree from './TreeContainer';
 import * as selectors from './selectors/nodes';
 import renderers from './renderers';
 import * as constants from './contants';
+import FilteringContainer from './FilteringContainer';
 
 export default Tree;
-export { selectors, renderers, constants };
+export { selectors, renderers, constants, FilteringContainer };

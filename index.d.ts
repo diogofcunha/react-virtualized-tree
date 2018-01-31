4bca2f5b20df240df2e7ed26dbbc747938387ae4
Diogo <diogofncunha@gmail.com>
2018-01-31 21:53:21 +0000

    Ts types (#19)

--- index.d.ts
+++ index.d.ts
@@ -1,85 +1,113 @@
 // Type definitions for react-virtualzed-tree
 // Definitions by: Diogo Cunha
 
-import * as ReactVirtualizedTree from 'react-virtualized-tree';
-
 import * as React from 'React';
 
-declare module 'ReactVirtualizedTree' {
-    interface BasicNode {
-      id: number | string;
-      name: string;
-      state?: { [s: key]: any };
-    }
-
-    export interface Node extends BasicNode {
-      children?: BasicNode[];
-    }
-
-    export interface FlattenedNode extends BasicNode {
-      deepness: number;
-      parents: number[];
-    }
-
-    type onChange = (updateParams: { node: FlattenedNode, type: string }) => void;
-
-    export interface Extensions {
-      updateTypeHandlers: { [type: number]: onChange }
-    }
-
-    export interface TreeProps {
-      extensions: Extensions;
-      nodes: Node[];
-      onChange: (nodes: Node[]) => void;
-      children: (props: RendererProps) => JSX.Element
-    }
-
-    export default class Tree extends React.Component<TreeProps> {}
-
-    export interface RendererProps {
-      onChange: onChange,
-      node: FlattenedNode
-    }
-
-    const Deletable: React.SFC<RendererProps>;
-    const Expandable: React.SFC<RendererProps>;
-    const Favorite: React.SFC<RendererProps>
-
-    export const renderers = {
-      Deletable,
-      Expandable,
-      Favorite
-    }
-
-    export interface Group {
-      filter: (node: Node) => boolean;
-      name: string
-    }
-
-    interface GroupRendererProps {
-      onChange: (c: string) => void;
-      groups: { [g: string]: Group };
-      selectedGroup: string;
-    }
-
-    export interface FilteringContainerProps {
-      children: (nodes: Node[]) => JSX.Element;
-      debouncer: (func: (...p: any[]) => any, timeout: number) => void;
-      groups: { [g: string]: Group };
-      selectedGroup: string;
-      groupRenderer: React.StatelessComponent<GroupRendererProps> | React.Component<GroupRendererProps>;
-      onSelectedGroupChange: (c: string) => void;
-    }
-
-    export class FilteringContainer extends React.Component<FilteringContainerProps> {}
-
-    export enum UPDATE_TYPE {
-      ADD = 0,
-      DELETE = 1,
-      UPDATE = 2
-    }
-
-    export const constants = {
-      UPDATE_TYPE
-    }
+interface BasicNode {
+  id: number | string;
+  name: string;
+  state?: { [stateKey: string]: any };
 }
+
+export interface Node extends BasicNode {
+  children?: BasicNode[];
+}
+
+export interface FlattenedNode extends Node {
+  deepness: number;
+  parents: number[];
+}
+
+interface NodeAction {
+  type: string;
+  node: FlattenedNode
+}
+
+type onChange = (updateParams: NodeAction) => void;
+
+export interface Extensions {
+  updateTypeHandlers: { [type: number]: onChange }
+}
+
+export interface TreeProps {
+  extensions: Extensions;
+  nodes: Node[];
+  onChange: (nodes: Node[]) => void;
+  children: (props: RendererProps) => JSX.Element
+}
+
+export default class Tree extends React.Component<TreeProps> {}
+
+export interface RendererProps {
+  onChange: onChange,
+  node: FlattenedNode
+}
+
+declare const Deletable: React.SFC<RendererProps>;
+declare const Expandable: React.SFC<RendererProps>;
+declare const Favorite: React.SFC<RendererProps>
+
+interface Renderers {
+  Deletable: React.SFC<RendererProps>;
+  Expandable: React.SFC<RendererProps>;
+  Favorite: React.SFC<RendererProps>;
+}
+
+export const renderers: Renderers;
+
+export interface Group {
+  filter: (node: Node) => boolean;
+  name: string
+}
+
+interface GroupRendererProps {
+  onChange: (c: string) => void;
+  groups: { [g: string]: Group };
+  selectedGroup: string;
+}
+
+export interface FilteringContainerProps {
+  children: (nodes: Node[]) => JSX.Element;
+  debouncer: (func: (...p: any[]) => any, timeout: number) => void;
+  groups: { [g: string]: Group };
+  selectedGroup: string;
+  groupRenderer: React.StatelessComponent<GroupRendererProps> | React.Component<GroupRendererProps>;
+  onSelectedGroupChange: (c: string) => void;
+}
+
+export class FilteringContainer extends React.Component<FilteringContainerProps> {}
+
+export enum UPDATE_TYPE {
+  ADD = 0,
+  DELETE = 1,
+  UPDATE = 2
+}
+
+interface Constants {
+  UPDATE_TYPE: UPDATE_TYPE
+}
+
+export const constants: Constants
+
+interface NodeRenderOptions {
+  hasChildren: boolean;
+  isExpanded: boolean;
+  isFavorite: boolean;
+  isDeletable: boolean;
+}
+
+export enum PICK_CHILDREN_FROM {
+  ORIGIN = 0,
+  CURRENT = 1
+}
+
+interface Selectors {
+  getNodeRenderOptions: (node: FlattenedNode) => NodeRenderOptions,
+  replaceNodeFromTree: (nodes: Node[], updatedNode: FlattenedNode, pickChildrenFrom?: PICK_CHILDREN_FROM) => Node[],
+  deleteNodeFromTree: (nodes: Node[], nodeToDelete: FlattenedNode) => Node[],
+  deleteNode: (node: FlattenedNode[]) => NodeAction,
+  addNode: (node: FlattenedNode[]) => NodeAction,
+  udpateNode: (node: FlattenedNode, state: { [stateKey: string]: any }) => NodeAction
+}
+
+export const selectors: Selectors;

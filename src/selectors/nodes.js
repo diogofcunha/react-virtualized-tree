a9410298529b25743323e6207d872bd2a89c4930
Diogo Cunha <diogofncunha@gmail.com>
2019-01-26 18:05:37 +0000

    feat: 🎸 added getNodeFromPath selector
    added a selector that based on an id path lookup's a node

--- src/selectors/nodes.js
+++ src/selectors/nodes.js
@@ -96,3 +96,40 @@ export const addNode = node => ({
 });
 
 export const getRowIndexFromId = (flattenedTree, id) => findIndex(flattenedTree, node => node.id === id);
+
+/**
+ * Gets a node in the original tree from a provided path.
+ *
+ * @param {number|string[]} path - The id path to the node
+ * @param {Object[]} tree - The Original tree
+ */
+export const getNodeFromPath = (path, tree) => {
+  let node;
+  let nextLevel = tree;
+
+  if (!Array.isArray(path)) {
+    throw new Error('path is not an array');
+  }
+
+  for (let i = 0; i < path.length; i++) {
+    const id = path[i];
+
+    let nextNode = nextLevel.find(n => n.id === id);
+
+    if (!nextNode) {
+      throw new Error(`Could not find node at ${path.join(',')}`);
+    }
+
+    if (i === path.length - 1 && nextNode.id === id) {
+      node = nextNode;
+    } else {
+      nextLevel = nextNode.children;
+    }
+  }
+
+  if (!node) {
+    throw new Error(`Could not find node at ${path.join(',')}`);
+  }
+
+  return node;
+};

7e76dce115fc81c1570e493b692e00410338bb8d
Diogo Cunha <diogofncunha@gmail.com>
2019-05-18 00:02:51 +0100

    test: 💍 minor change in exposed mock data

--- testData/sampleTree.js
+++ testData/sampleTree.js
@@ -1,47 +1,49 @@
-export const Nodes = [
-  {
-    id: 0,
-    name: 'Leaf 1',
-    state: {
-      expanded: true,
-    },
-    children: [
-      {
-        id: 2,
-        name: 'Leaf 2',
-        state: {
-          expanded: true,
-          deletable: true,
-        },
-        children: [
-          {
-            id: 3,
-            name: 'Leaf 3',
-            state: {
-              expanded: false,
-              favorite: true,
-              deletable: true,
-            },
-            children: [
-              {
-                id: 'c-3',
-                name: 'Leaf 3 Child',
-                state: {},
-              },
-            ],
-          },
-          {
-            id: 4,
-            name: 'Leaf 4',
-          },
-        ],
-      },
-      {
-        id: 5,
-        name: 'Leaf 5',
-      },
-    ],
+export const EXPANDED_NODE_IN_ROOT = {
+  id: 0,
+  name: 'Leaf 1',
+  state: {
+    expanded: true,
   },
+  children: [
+    {
+      id: 2,
+      name: 'Leaf 2',
+      state: {
+        expanded: true,
+        deletable: true,
+      },
+      children: [
+        {
+          id: 3,
+          name: 'Leaf 3',
+          state: {
+            expanded: false,
+            favorite: true,
+            deletable: true,
+          },
+          children: [
+            {
+              id: 'c-3',
+              name: 'Leaf 3 Child',
+              state: {},
+            },
+          ],
+        },
+        {
+          id: 4,
+          name: 'Leaf 4',
+        },
+      ],
+    },
+    {
+      id: 5,
+      name: 'Leaf 5',
+    },
+  ],
+};
+
+export const Nodes = [
+  EXPANDED_NODE_IN_ROOT,
   {
     id: 1,
     name: 'Leaf 6',

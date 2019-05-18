1b3e37803e1e6ac71c74c2893b75b420e81cf6ed
Diogo Cunha <diogofncunha@gmail.com>
2019-05-18 09:56:07 +0100

    test: 💍 added Deletable tests and small restructure

--- src/__tests__/Tree-with-renderers.test.js
+++ src/__tests__/Tree-with-renderers.test.js
@@ -10,12 +10,16 @@ import {
   EXPANDED_CHILDREN,
   COLLAPSED_NODE_IN_ROOT,
   COLLAPSED_CHILDREN,
+  DELETABLE_IN_ROOT,
+  DELETABLE_CHILDREN,
 } from '../../testData/sampleTree';
+import NodeDiff from '../../tests/NodeDiff';
 
 const {Expandable, Deletable, Favorite} = renderers;
 
 const EXPAND_ICON_CN = 'EXPAND';
 const COLLAPSE_ICON_CN = 'COLLAPSE';
+const DELETE_ICON_CN = 'DELETE';
 
 class Example extends React.Component {
   state = {
@@ -30,7 +34,7 @@ class Example extends React.Component {
     return (
       <Tree nodes={this.state.nodes} onChange={this.handleChange}>
         {({style, node, ...rest}) => (
-          <div style={style} data-testid={node.id}>
+          <div style={style} data-testid={node.id} data-type="node">
             <Expandable
               node={node}
               {...rest}
@@ -40,7 +44,7 @@ class Example extends React.Component {
               }}
             >
               {node.name}
-              <Deletable node={node} {...rest}>
+              <Deletable node={node} {...rest} iconsClassNameMap={{delete: DELETE_ICON_CN}}>
                 <Favorite node={node} {...rest} />
               </Deletable>
             </Expandable>
@@ -55,104 +59,302 @@ describe('Tree with exposed renderers', () => {
   afterEach(cleanup);
 
   describe('Expandable', () => {
-    test('should be able to collapse a node in the root', () => {
-      const {getByTestId} = render(<Example />);
+    describe('when collapsing a node in the root', () => {
+      test('should change the icon from expand to collapse', () => {
+        const {getByTestId} = render(<Example />);
+        const {id} = EXPANDED_NODE_IN_ROOT;
 
-      const {id, children} = EXPANDED_NODE_IN_ROOT;
+        let targetExpandedNode = getByTestId(`${id}`);
+        expect(targetExpandedNode.querySelector(`.${COLLAPSE_ICON_CN}`)).toBeNull();
 
-      let renderedNode = getByTestId(`${id}`);
-      expect(renderedNode.querySelector(`.${COLLAPSE_ICON_CN}`)).toBeNull();
-      children.forEach(c => {
-        expect(getByTestId(`${c.id}`)).toBeDefined();
+        fireEvent.click(targetExpandedNode.querySelector(`.${EXPAND_ICON_CN}`));
+
+        targetExpandedNode = getByTestId(`${id}`);
+        expect(targetExpandedNode.querySelector(`.${COLLAPSE_ICON_CN}`)).not.toBeNull();
+        expect(targetExpandedNode.querySelector(`.${EXPAND_ICON_CN}`)).toBeNull();
       });
 
-      // Collapse node by clicking the expand icon.
-      const collapsedNode = renderedNode.querySelector(`.${EXPAND_ICON_CN}`);
-      fireEvent.click(collapsedNode);
+      test('should hide the children nodes', () => {
+        const {getByTestId} = render(<Example />);
+        const {id, children} = EXPANDED_NODE_IN_ROOT;
 
-      // Children should now disappear and icon should change.
-      children.forEach(c => {
-        expect(() => getByTestId(`${c.id}`)).toThrow();
+        children.forEach(c => {
+          expect(getByTestId(`${c.id}`)).toBeDefined();
+        });
+
+        let targetExpandedNode = getByTestId(`${id}`);
+        fireEvent.click(targetExpandedNode.querySelector(`.${EXPAND_ICON_CN}`));
+
+        children.forEach(c => {
+          expect(() => getByTestId(`${c.id}`)).toThrow();
+        });
       });
 
-      renderedNode = getByTestId(`${id}`);
-      expect(renderedNode.querySelector(`.${COLLAPSE_ICON_CN}`)).not.toBeNull();
-      expect(renderedNode.querySelector(`.${EXPAND_ICON_CN}`)).toBeNull();
+      test('should affect the expected nodes', () => {
+        const {getByTestId, container} = render(<Example />);
+        const {id} = EXPANDED_NODE_IN_ROOT;
+        const diff = new NodeDiff(container);
+
+        let targetExpandedNode = getByTestId(`${id}`);
+        fireEvent.click(targetExpandedNode.querySelector(`.${EXPAND_ICON_CN}`));
+
+        expect(diff.run(container)).toMatchInlineSnapshot(`
+                                                            Object {
+                                                              "changed": Array [
+                                                                "0",
+                                                              ],
+                                                              "mounted": Array [],
+                                                              "unmounted": Array [
+                                                                "2",
+                                                                "3",
+                                                                "4",
+                                                                "5",
+                                                              ],
+                                                            }
+                                                `);
+      });
     });
 
-    test('should be able to collapse a node deep', () => {
-      const {getByTestId} = render(<Example />);
+    describe('when collapsing a node deep within', () => {
+      test('should change the icon from expand to collapse', () => {
+        const {getByTestId} = render(<Example />);
+        const {id} = EXPANDED_CHILDREN;
 
-      const {id, children} = EXPANDED_CHILDREN;
+        let targetExpandedNode = getByTestId(`${id}`);
+        expect(targetExpandedNode.querySelector(`.${COLLAPSE_ICON_CN}`)).toBeNull();
 
-      let renderedNode = getByTestId(`${id}`);
-      expect(renderedNode.querySelector(`.${COLLAPSE_ICON_CN}`)).toBeNull();
-      children.forEach(c => {
-        expect(getByTestId(`${c.id}`)).toBeDefined();
+        fireEvent.click(targetExpandedNode.querySelector(`.${EXPAND_ICON_CN}`));
+
+        targetExpandedNode = getByTestId(`${id}`);
+        expect(targetExpandedNode.querySelector(`.${COLLAPSE_ICON_CN}`)).not.toBeNull();
+        expect(targetExpandedNode.querySelector(`.${EXPAND_ICON_CN}`)).toBeNull();
       });
 
-      // Collapse node by clicking the expand icon.
-      const collapsedNode = renderedNode.querySelector(`.${EXPAND_ICON_CN}`);
-      fireEvent.click(collapsedNode);
+      test('should hide the children nodes', () => {
+        const {getByTestId} = render(<Example />);
+        const {id, children} = EXPANDED_CHILDREN;
 
-      // Children should now disappear and icon should change.
-      children.forEach(c => {
-        expect(() => getByTestId(`${c.id}`)).toThrow();
+        children.forEach(c => {
+          expect(getByTestId(`${c.id}`)).toBeDefined();
+        });
+
+        let targetExpandedNode = getByTestId(`${id}`);
+        fireEvent.click(targetExpandedNode.querySelector(`.${EXPAND_ICON_CN}`));
+
+        children.forEach(c => {
+          expect(() => getByTestId(`${c.id}`)).toThrow();
+        });
       });
 
-      renderedNode = getByTestId(`${id}`);
-      expect(renderedNode.querySelector(`.${COLLAPSE_ICON_CN}`)).not.toBeNull();
-      expect(renderedNode.querySelector(`.${EXPAND_ICON_CN}`)).toBeNull();
+      test('should affect the expected nodes', () => {
+        const {getByTestId, container} = render(<Example />);
+        const {id} = EXPANDED_CHILDREN;
+        const diff = new NodeDiff(container);
+
+        let targetExpandedNode = getByTestId(`${id}`);
+        fireEvent.click(targetExpandedNode.querySelector(`.${EXPAND_ICON_CN}`));
+
+        expect(diff.run(container)).toMatchInlineSnapshot(`
+                                                  Object {
+                                                    "changed": Array [
+                                                      "2",
+                                                    ],
+                                                    "mounted": Array [],
+                                                    "unmounted": Array [
+                                                      "3",
+                                                      "4",
+                                                    ],
+                                                  }
+                                        `);
+      });
     });
 
-    test('should be able to expand a node in the root', () => {
-      const {getByTestId} = render(<Example />);
+    describe('when expanding a node in the root', () => {
+      test('should change the icon from collapse to expand', () => {
+        const {getByTestId} = render(<Example />);
+        const {id} = COLLAPSED_NODE_IN_ROOT;
 
-      const {id, children} = COLLAPSED_NODE_IN_ROOT;
+        let targetExpandedNode = getByTestId(`${id}`);
+        expect(targetExpandedNode.querySelector(`.${EXPAND_ICON_CN}`)).toBeNull();
 
-      let renderedNode = getByTestId(`${id}`);
-      expect(renderedNode.querySelector(`.${EXPAND_ICON_CN}`)).toBeNull();
-      children.forEach(c => {
-        expect(() => getByTestId(`${c.id}`)).toThrow();
+        fireEvent.click(targetExpandedNode.querySelector(`.${COLLAPSE_ICON_CN}`));
+
+        targetExpandedNode = getByTestId(`${id}`);
+        expect(targetExpandedNode.querySelector(`.${EXPAND_ICON_CN}`)).not.toBeNull();
+        expect(targetExpandedNode.querySelector(`.${COLLAPSE_ICON_CN}`)).toBeNull();
       });
 
-      // Expand node by clicking the collapse icon.
-      const collapsedNode = renderedNode.querySelector(`.${COLLAPSE_ICON_CN}`);
-      fireEvent.click(collapsedNode);
+      test('should show the children nodes', () => {
+        const {getByTestId} = render(<Example />);
+        const {id, children} = COLLAPSED_NODE_IN_ROOT;
 
-      // Children should now appear and icon should change.
-      children.forEach(c => {
-        expect(getByTestId(`${c.id}`)).toBeDefined();
+        children.forEach(c => {
+          expect(() => getByTestId(`${c.id}`)).toThrow();
+        });
+
+        let targetExpandedNode = getByTestId(`${id}`);
+        fireEvent.click(targetExpandedNode.querySelector(`.${COLLAPSE_ICON_CN}`));
+
+        children.forEach(c => {
+          expect(getByTestId(`${c.id}`)).toBeDefined();
+        });
       });
 
-      renderedNode = getByTestId(`${id}`);
-      expect(renderedNode.querySelector(`.${EXPAND_ICON_CN}`)).not.toBeNull();
-      expect(renderedNode.querySelector(`.${COLLAPSE_ICON_CN}`)).toBeNull();
+      test('should affect the expected nodes', () => {
+        const {getByTestId, container} = render(<Example />);
+        const {id} = COLLAPSED_NODE_IN_ROOT;
+        const diff = new NodeDiff(container);
+
+        let targetExpandedNode = getByTestId(`${id}`);
+        fireEvent.click(targetExpandedNode.querySelector(`.${COLLAPSE_ICON_CN}`));
+
+        expect(diff.run(container)).toMatchInlineSnapshot(`
+                                        Object {
+                                          "changed": Array [
+                                            "1",
+                                          ],
+                                          "mounted": Array [
+                                            "6",
+                                            "9",
+                                          ],
+                                          "unmounted": Array [],
+                                        }
+                                `);
+      });
     });
 
-    test('should be able to expand a node deep', () => {
-      const {getByTestId} = render(<Example />);
+    describe('when expanding a node deep within', () => {
+      test('should change the icon from collapse to expand', () => {
+        const {getByTestId} = render(<Example />);
+        const {id} = COLLAPSED_CHILDREN;
 
-      const {id, children} = COLLAPSED_CHILDREN;
+        let targetExpandedNode = getByTestId(`${id}`);
+        expect(targetExpandedNode.querySelector(`.${EXPAND_ICON_CN}`)).toBeNull();
 
-      let renderedNode = getByTestId(`${id}`);
-      expect(renderedNode.querySelector(`.${EXPAND_ICON_CN}`)).toBeNull();
-      children.forEach(c => {
-        expect(() => getByTestId(`${c.id}`)).toThrow();
+        fireEvent.click(targetExpandedNode.querySelector(`.${COLLAPSE_ICON_CN}`));
+
+        targetExpandedNode = getByTestId(`${id}`);
+        expect(targetExpandedNode.querySelector(`.${EXPAND_ICON_CN}`)).not.toBeNull();
+        expect(targetExpandedNode.querySelector(`.${COLLAPSE_ICON_CN}`)).toBeNull();
       });
 
-      // Expand node by clicking the collapse icon.
-      const collapsedNode = renderedNode.querySelector(`.${COLLAPSE_ICON_CN}`);
-      fireEvent.click(collapsedNode);
+      test('should show the children nodes', () => {
+        const {getByTestId} = render(<Example />);
+        const {id, children} = COLLAPSED_CHILDREN;
 
-      // Children should now appear and icon should change.
-      children.forEach(c => {
-        expect(getByTestId(`${c.id}`)).toBeDefined();
+        children.forEach(c => {
+          expect(() => getByTestId(`${c.id}`)).toThrow();
+        });
+
+        let targetExpandedNode = getByTestId(`${id}`);
+        fireEvent.click(targetExpandedNode.querySelector(`.${COLLAPSE_ICON_CN}`));
+
+        children.forEach(c => {
+          expect(getByTestId(`${c.id}`)).toBeDefined();
+        });
       });
 
-      renderedNode = getByTestId(`${id}`);
-      expect(renderedNode.querySelector(`.${EXPAND_ICON_CN}`)).not.toBeNull();
-      expect(renderedNode.querySelector(`.${COLLAPSE_ICON_CN}`)).toBeNull();
+      test('should affect the expected nodes', () => {
+        const {getByTestId, container} = render(<Example />);
+        const {id} = COLLAPSED_CHILDREN;
+        const diff = new NodeDiff(container);
+
+        let targetExpandedNode = getByTestId(`${id}`);
+        fireEvent.click(targetExpandedNode.querySelector(`.${COLLAPSE_ICON_CN}`));
+
+        expect(diff.run(container)).toMatchInlineSnapshot(`
+                              Object {
+                                "changed": Array [
+                                  "3",
+                                ],
+                                "mounted": Array [
+                                  "c-3",
+                                ],
+                                "unmounted": Array [],
+                              }
+                        `);
+      });
+    });
+  });
+
+  describe('Deletable', () => {
+    describe('when deleting a node in the root', () => {
+      test('should delete the node', () => {
+        const {getByTestId} = render(<Example />);
+
+        const {id} = DELETABLE_IN_ROOT;
+
+        let renderedNode = getByTestId(`${id}`);
+        const collapsedNode = renderedNode.querySelector(`.${DELETE_ICON_CN}`);
+        fireEvent.click(collapsedNode);
+
+        expect(() => getByTestId(`${id}`)).toThrow();
+      });
+
+      test('should affect the expected nodes', () => {
+        const {getByTestId, container} = render(<Example />);
+        const diff = new NodeDiff(container);
+
+        const {id} = DELETABLE_IN_ROOT;
+
+        let renderedNode = getByTestId(`${id}`);
+        const collapsedNode = renderedNode.querySelector(`.${DELETE_ICON_CN}`);
+        fireEvent.click(collapsedNode);
+
+        expect(diff.run(container)).toMatchInlineSnapshot(`
+                    Object {
+                      "changed": Array [],
+                      "mounted": Array [],
+                      "unmounted": Array [
+                        "z",
+                      ],
+                    }
+                `);
+      });
+    });
+
+    describe('when deleting a node deep within', () => {
+      test('should delete the node and the children', () => {
+        const {getByTestId} = render(<Example />);
+
+        const {id, children} = DELETABLE_CHILDREN;
+        children.forEach(c => {
+          expect(getByTestId(`${c.id}`)).toBeDefined();
+        });
+
+        let renderedNode = getByTestId(`${id}`);
+        const collapsedNode = renderedNode.querySelector(`.${DELETE_ICON_CN}`);
+        fireEvent.click(collapsedNode);
+
+        expect(() => getByTestId(`${id}`)).toThrow();
+
+        children.forEach(c => {
+          expect(() => getByTestId(`${c.id}`)).toThrow();
+        });
+      });
+
+      test('should affect the expected nodes', () => {
+        const {getByTestId, container} = render(<Example />);
+        const diff = new NodeDiff(container);
+
+        const {id} = DELETABLE_CHILDREN;
+
+        let renderedNode = getByTestId(`${id}`);
+        const collapsedNode = renderedNode.querySelector(`.${DELETE_ICON_CN}`);
+        fireEvent.click(collapsedNode);
+
+        expect(diff.run(container)).toMatchInlineSnapshot(`
+          Object {
+            "changed": Array [],
+            "mounted": Array [],
+            "unmounted": Array [
+              "2",
+              "3",
+              "4",
+            ],
+          }
+        `);
+      });
     });
   });
 });

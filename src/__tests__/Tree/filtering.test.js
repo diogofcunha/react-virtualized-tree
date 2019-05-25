a6267dcdfb2dbf1546be2c06bc27921f620d2b46
Diogo Cunha <diogofncunha@gmail.com>
2019-05-25 17:31:18 +0100

    test: 💍 finished filtering integration tests

--- src/__tests__/Tree/filtering.test.js
+++ src/__tests__/Tree/filtering.test.js
@@ -1,11 +1,10 @@
 jest.mock('react-virtualized');
 
 import React from 'react';
-import {render, cleanup} from 'react-testing-library';
+import {render, cleanup, fireEvent} from 'react-testing-library';
 import ReactTestUtils from 'react-dom/test-utils';
 
 import Tree, {renderers} from '../..';
-import NodeDiff from '../../../tests/NodeDiff';
 import FilteringContainer from '../../FilteringContainer';
 
 const {Expandable} = renderers;
@@ -25,6 +24,7 @@ class Example extends React.Component {
             name: 'react',
             state: {
               expanded: true,
+              tags: ['javascript', 'open-source'],
             },
           },
           {
@@ -32,6 +32,7 @@ class Example extends React.Component {
             name: 'graphql',
             state: {
               expanded: true,
+              tags: ['open-source', 'api'],
             },
           },
           {
@@ -39,6 +40,7 @@ class Example extends React.Component {
             name: 'jest',
             state: {
               expanded: true,
+              tags: ['javascript', 'open-source'],
             },
           },
         ],
@@ -55,6 +57,23 @@ class Example extends React.Component {
             name: 'chrome',
             state: {
               expanded: true,
+              tags: ['browser'],
+            },
+          },
+          {
+            id: 21,
+            name: 'chromium',
+            state: {
+              expanded: true,
+              tags: ['browser', 'open-source'],
+            },
+          },
+          {
+            id: 20,
+            name: 'chromium os',
+            state: {
+              expanded: true,
+              tags: ['browser', 'open-source'],
             },
           },
           {
@@ -62,6 +81,7 @@ class Example extends React.Component {
             name: 'angular',
             state: {
               expanded: true,
+              tags: ['javascript', 'open-source'],
             },
           },
           {
@@ -69,6 +89,7 @@ class Example extends React.Component {
             name: 'google-cloud',
             state: {
               expanded: true,
+              tags: ['cloud'],
             },
           },
         ],
@@ -85,6 +106,7 @@ class Example extends React.Component {
             name: 'typescript',
             state: {
               expanded: true,
+              tags: ['javascript', 'open-source'],
             },
           },
           {
@@ -92,6 +114,7 @@ class Example extends React.Component {
             name: 'vscode',
             state: {
               expanded: true,
+              tags: ['code-editor'],
             },
           },
           {
@@ -99,6 +122,7 @@ class Example extends React.Component {
             name: 'azure',
             state: {
               expanded: true,
+              tags: ['cloud'],
             },
           },
         ],
@@ -120,10 +144,12 @@ class Example extends React.Component {
               {
                 id: 14,
                 name: 'google-cloud',
+                tags: ['cloud'],
               },
               {
                 id: 15,
                 name: 'azure',
+                tags: ['cloud'],
               },
             ],
           },
@@ -137,29 +163,43 @@ class Example extends React.Component {
               {
                 id: 17,
                 name: 'react',
+                tags: ['javascript', 'open-source'],
               },
               {
                 id: 18,
                 name: 'vue',
+                tags: ['javascript', 'open-source'],
               },
               {
                 id: 19,
                 name: 'angular',
+                tags: ['javascript', 'open-source'],
               },
             ],
           },
         ],
       },
     ],
+    selectedGroup: undefined,
   };
 
   handleChange = nodes => {
     this.setState({nodes});
   };
 
+  handleSelectedGroupChange = selectedGroup => {
+    this.setState({selectedGroup});
+  };
+
   render() {
     return (
-      <FilteringContainer nodes={this.state.nodes} debouncer={v => v}>
+      <FilteringContainer
+        nodes={this.state.nodes}
+        debouncer={v => v}
+        selectedGroup={this.state.selectedGroup}
+        onSelectedGroupChange={this.handleSelectedGroupChange}
+        {...this.props}
+      >
         {({nodes}) => (
           <Tree nodes={nodes} onChange={this.handleChange}>
             {({style, node, ...rest}) => (
@@ -179,8 +219,19 @@ class Example extends React.Component {
 describe('Tree with filtering container', () => {
   afterEach(cleanup);
 
-  const setup = () => {
-    const wrapper = render(<Example />);
+  const getRenderedNodesNames = container => {
+    const nodes = container.querySelectorAll('[data-type="node"]');
+
+    const names = [];
+    nodes.forEach(n => {
+      names.push(n.textContent);
+    });
+
+    return names;
+  };
+
+  const setup = (props = {}) => {
+    const wrapper = render(<Example {...props} />);
 
     return {
       search: value => {
@@ -207,96 +258,127 @@ describe('Tree with filtering container', () => {
 
     test('should render as expected when trying to search for a node in the root', () => {
       const {wrapper, search} = setup();
-      const diff = new NodeDiff(wrapper.container);
 
       search('Top Microsoft');
 
-      expect(diff.run(wrapper.container)).toMatchInlineSnapshot(`
-                        Object {
-                          "changed": Array [
-                            "8",
-                          ],
-                          "mounted": Array [],
-                          "unmounted": Array [
-                            "0",
-                            "1",
-                            "2",
-                            "3",
-                            "4",
-                            "5",
-                            "6",
-                            "7",
-                            "9",
-                            "10",
-                            "11",
-                            "12",
-                            "13",
-                            "16",
-                            "17",
-                            "18",
-                            "19",
-                          ],
-                        }
-                  `);
+      expect(getRenderedNodesNames(wrapper.container)).toMatchInlineSnapshot(`
+        Array [
+          "Top Microsoft Eng Tags",
+        ]
+      `);
     });
 
     test('should render as expected when trying to search for nodes expanded deep within', () => {
       const {wrapper, search} = setup();
-      const diff = new NodeDiff(wrapper.container);
-
       search('react');
 
-      expect(diff.run(wrapper.container)).toMatchInlineSnapshot(`
-                Object {
-                  "changed": Array [],
-                  "mounted": Array [],
-                  "unmounted": Array [
-                    "2",
-                    "3",
-                    "4",
-                    "5",
-                    "6",
-                    "7",
-                    "8",
-                    "9",
-                    "10",
-                    "11",
-                    "13",
-                    "18",
-                    "19",
-                  ],
-                }
+      expect(getRenderedNodesNames(wrapper.container)).toMatchInlineSnapshot(`
+                Array [
+                  "Top Facebook Eng Tags",
+                  "react",
+                  "Top Global Eng Tags",
+                  "front-end",
+                  "react",
+                ]
             `);
     });
 
     test('should render as expected when trying to search for nodes collapsed deep within', () => {
       const {wrapper, search} = setup();
-      const diff = new NodeDiff(wrapper.container);
 
       search('google-cloud');
 
-      expect(diff.run(wrapper.container)).toMatchInlineSnapshot(`
-        Object {
-          "changed": Array [],
-          "mounted": Array [],
-          "unmounted": Array [
-            "0",
-            "1",
-            "2",
-            "3",
-            "5",
-            "6",
-            "8",
-            "9",
-            "10",
-            "11",
-            "16",
-            "17",
-            "18",
-            "19",
-          ],
-        }
-      `);
+      expect(getRenderedNodesNames(wrapper.container)).toMatchInlineSnapshot(`
+                Array [
+                  "Top Google Eng Tags",
+                  "google-cloud",
+                  "Top Global Eng Tags",
+                  "clouds",
+                ]
+            `);
+    });
+  });
+
+  describe('with groups', () => {
+    const setupWithGroups = () => {
+      const rendered = setup({
+        groups: {
+          JS: {
+            name: 'JS',
+            filter: ({state = {}}) => {
+              const {tags = []} = state;
+
+              return tags.includes('javascript');
+            },
+          },
+          OPEN_SOURCE: {
+            name: 'OPEN_SOURCE',
+            filter: ({state = {}}) => {
+              const {tags = []} = state;
+
+              return tags.includes('open-source');
+            },
+          },
+        },
+      });
+
+      return {
+        ...rendered,
+        selectGroup: value => {
+          const {container} = rendered.wrapper;
+
+          const select = container.querySelector('select');
+          select.value = value;
+
+          fireEvent.change(select);
+        },
+      };
+    };
+
+    test('should render and filter as expected when selecting a group', () => {
+      const {wrapper, selectGroup} = setupWithGroups();
+
+      selectGroup('JS');
+
+      expect(getRenderedNodesNames(wrapper.container)).toMatchInlineSnapshot(`
+                        Array [
+                          "Top Facebook Eng Tags",
+                          "react",
+                          "jest",
+                          "Top Google Eng Tags",
+                          "angular",
+                          "Top Microsoft Eng Tags",
+                          "typescript",
+                        ]
+                  `);
+    });
+
+    test('should render an empty tree when search does not match any node in the group', () => {
+      const {wrapper, search, selectGroup} = setupWithGroups();
+
+      // Before selecting the group the search should return
+      search('azure');
+      expect(wrapper.container.querySelector('[data-type="node"]')).not.toBeNull();
+
+      selectGroup('JS');
+      search('azure');
+
+      expect(wrapper.container.querySelector('[data-type="node"]')).toBeNull();
+    });
+
+    test('should render and filter a tree correctly using the group and search term', () => {
+      const {wrapper, search, selectGroup} = setupWithGroups();
+
+      selectGroup('OPEN_SOURCE');
+      search('chrom');
+
+      expect(getRenderedNodesNames(wrapper.container)).toMatchInlineSnapshot(`
+                                Array [
+                                  "Top Google Eng Tags",
+                                  "chromium",
+                                  "chromium os",
+                                ]
+                        `);
     });
   });
 });

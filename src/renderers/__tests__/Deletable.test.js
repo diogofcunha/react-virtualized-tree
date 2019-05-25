a8ee0ae061205d232916b82ecae0652fcec9a4a5
Diogo Cunha <diogofncunha@gmail.com>
2019-05-25 08:54:12 +0100

    test: 💍 moved Deletable renderer tests to react-testing-library

--- src/renderers/__tests__/Deletable.test.js
+++ src/renderers/__tests__/Deletable.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {render, cleanup, fireEvent} from 'react-testing-library';
 
 import Deletable from '../Deletable';
 import {KEY_CODES} from '../../eventWrappers';
 import {deleteNode} from '../../selectors/nodes';
 
 describe('renderers Deletable', () => {
-  const findDeleteIcon = wrapper => wrapper.find('i');
+  afterEach(cleanup);
 
   const setup = (state = {deletable: true}, extraProps = {}) => {
     const baseProps = {
@@ -18,38 +18,51 @@ describe('renderers Deletable', () => {
         deepness: 0,
         children: [{}],
       },
-      iconsClassNameMap: {
-        delete: 'delete',
-      },
       measure: jest.fn(),
       index: 1,
     };
 
     const props = {...baseProps, ...extraProps};
-    const wrapper = shallow(<Deletable {...props} />);
 
-    return {props, wrapper, deleteIconWrapper: findDeleteIcon(wrapper)};
+    return {
+      ...render(<Deletable {...props} />),
+      props,
+    };
   };
 
   describe('when it is deletable', () => {
-    it('should render the delete icon with the supplied className or the default', () => {
-      const {deleteIconWrapper, props} = setup();
+    it('should render the delete icon with the supplied className', () => {
+      const {container, props} = setup(
+        {deletable: true},
+        {
+          iconsClassNameMap: {
+            delete: 'delete',
+          },
+        },
+      );
 
-      expect(deleteIconWrapper.hasClass(props.iconsClassNameMap.delete)).toBeTruthy();
+      expect(container.querySelector(`.${props.iconsClassNameMap.delete}`)).not.toBeNull();
+      expect(container.querySelector(`.mi.mi-delete`)).toBeNull();
+    });
+
+    it('should render the delete icon with the default className when one is not supplied', () => {
+      const {container} = setup();
+
+      expect(container.querySelector(`.mi.mi-delete`)).not.toBeNull();
     });
 
     it('clicking should call onChange with the correct params', () => {
-      const {deleteIconWrapper, props} = setup();
+      const {container, props} = setup();
 
-      deleteIconWrapper.simulate('click');
+      fireEvent.click(container.querySelector('i'));
 
       expect(props.onChange).toHaveBeenCalledWith({...deleteNode(props.node), index: props.index});
     });
 
     it('pressing enter should call onChange with the correct params', () => {
-      const {deleteIconWrapper, props} = setup();
+      const {props, container} = setup();
 
-      deleteIconWrapper.simulate('keyDown', {keyCode: KEY_CODES.Enter});
+      fireEvent.keyDown(container.querySelector('i'), {keyCode: KEY_CODES.Enter});
 
       expect(props.onChange).toHaveBeenCalledWith({...deleteNode(props.node), index: props.index});
     });
@@ -57,9 +70,9 @@ describe('renderers Deletable', () => {
 
   describe('when it is not deletable', () => {
     it('should net render delete icon', () => {
-      const {deleteIconWrapper} = setup({deletable: false});
+      const {container} = setup({deletable: false});
 
-      expect(deleteIconWrapper.length).toBe(0);
+      expect(container.querySelector('i')).toBeNull();
     });
   });
 });

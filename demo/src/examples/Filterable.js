214cd00d5b2083224849cced04f4cc28f7dfbf0b
AzureAD\JoshHunt <josh.hunt@cin7.com>
2020-10-23 09:09:13 +1300

    Remove test data

--- demo/src/examples/Filterable.js
+++ demo/src/examples/Filterable.js
@@ -70,7 +70,7 @@ class Filterable extends Component {
           onChange={this.handleGroupsToogle}
           style={{marginBottom: 15}}
         />
-        <FilteringContainer nodes={this.state.nodes} {...this._groupProps} placeholder="Hi Sherwin">
+        <FilteringContainer nodes={this.state.nodes} {...this._groupProps}>
           {({nodes}) => (
             <div style={{height: 500}}>
               <Tree nodes={nodes} onChange={this.handleChange}>

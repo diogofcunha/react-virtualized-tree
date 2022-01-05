54322aa0bb4ac9e8401ee250e066272c4ebf2e54
Kovalchuk Volodymyr <vladimir.kovalchuk@gmail.com>
2022-01-05 21:23:51 +0200

    Update getFlattenedTree for better performance

--- src/selectors/getFlattenedTree.js
+++ src/selectors/getFlattenedTree.js
@@ -1,17 +1,39 @@
 export const isNodeExpanded = node => node.state && node.state.expanded;
 export const nodeHasChildren = node => node.children && node.children.length;
 
-export const getFlattenedTree = (nodes, parents = []) =>
-  nodes.reduce((flattenedTree, node) => {
-    const deepness = parents.length;
-    const nodeWithHelpers = {...node, deepness, parents};
+export const getFlattenedTree = (nodes, parents = []) => {
+  const result = [];
+  const flattenNodes = [];
+  // add in reverse order
+  for (let i = nodes.length - 1; i >= 0; i--) {
+    flattenNodes.push({
+      node: nodes[i],
+      parents,
+    });
+  }
 
-    if (!nodeHasChildren(node) || !isNodeExpanded(node)) {
-      return [...flattenedTree, nodeWithHelpers];
+  while (flattenNodes.length > 0) {
+    const { node, parents } = flattenNodes.pop();
+    // add node with helpers
+    result.push({
+      ...node,
+      deepness: parents.length,
+      parents,
+    });
+
+    if (nodeHasChildren(node) && isNodeExpanded(node)) {
+      const childParents = [...parents, node.id];
+      // add all children in reverse order
+      for (let i = node.children.length - 1; i >= 0; i--) {
+        flattenNodes.push({
+          node: node.children[i],
+          parents: childParents,
+        });
+      }
     }
-
-    return [...flattenedTree, nodeWithHelpers, ...getFlattenedTree(node.children, [...parents, node.id])];
-  }, []);
+  }
+  return result;
+}
 
 export const getFlattenedTreePaths = (nodes, parents = []) => {
   const paths = [];
